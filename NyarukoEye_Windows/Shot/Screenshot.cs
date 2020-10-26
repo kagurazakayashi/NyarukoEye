1cb346aaeebc2beff4f035dc6331b1b2449fe2d3
神楽坂雅詩 <cyscxchope@gmail.com>
2020-10-26 18:55:28 +0800

    win:分离屏幕截图模块

---
+++ NyarukoEye_Windows/Shot/Screenshot.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace NyarukoEye_Windows
+{
+    static public class Screenshot
+    {
+        static public void saveScreenshot(string filePath, ImageFormat imageFormat)
+        {
+            int screenW = Screen.PrimaryScreen.Bounds.Width;
+            int screenH = Screen.PrimaryScreen.Bounds.Height;
+            Bitmap bmp = new Bitmap(screenW, screenH);
+            Graphics gra = Graphics.FromImage(bmp);
+            gra.CopyFromScreen(new Point(0, 0), new Point(0, 0), bmp.Size);
+            bmp.Save(filePath, imageFormat);
+            bmp.Dispose();
+            gra.Dispose();
+        }
+    }
+}
