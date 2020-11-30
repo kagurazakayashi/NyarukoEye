781d73ae85e4a0490f851041677d03292fdd0305
神楽坂雅詩 <cyscxchope@gmail.com>
2020-11-30 18:40:31 +0800

    win:添加或删除模块程序

---
+++ NyarukoEye_Windows/Installer/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(93, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(583, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NyarukoEye 聚合审计工具 v1.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.checkedListBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "安装服务器端软件",
+            "安装客户端软件",
+            "客户端配置编辑器",
+            "服务器端配置编辑器",
+            "开机启动配置工具",
+            "密钥和加密测试工具",
+            "网络上传测试工具"});
+            this.checkedListBox1.Location = new System.Drawing.Point(93, 294);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(583, 198);
+            this.checkedListBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(93, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(583, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "功能 》";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(93, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(583, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "位置 》";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.textBox1.Location = new System.Drawing.Point(93, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(583, 33);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(299, 539);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 45);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "删除以上功能(&D)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(514, 539);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 45);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "退出(&E)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "请选择安装文件夹";
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(94, 539);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 45);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "安装以上功能(&S)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.Location = new System.Drawing.Point(93, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(583, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "安装程序";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(776, 632);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NyarukoEye 聚合审计工具 v1.0 安装程序";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NyarukoEye_Windows/Installer/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Installer
+{
+    public partial class Form1 : Form
+    {
+        string[] dlls = { "Enc.dll", "INI.dll", "NetUL.dll", "RestSharp.dll", "Shot.dll" };
+        string[] exes = { "nyarukoeyesev.exe", "NELauncher.exe", "scrchk.exe", "ConfigEditer.exe", "ConfigEditerServer.exe", "Autorun.exe", "KeyTool.exe", "NetTool.exe" };
+
+        private List<string> stat = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string dir = folderBrowserDialog1.SelectedPath + "\\";
+                if (Directory.Exists(dir))
+                {
+                    textBox1.Text = dir;
+                    button1.Visible = button3.Visible = true;
+                }
+                else
+                {
+                    button1.Visible = button3.Visible = false;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stat.Clear();
+            try
+            {
+                installFile(0, "nyarukoeyesev.exe");
+                installFile(1, "NELauncher.exe");
+                installFile(1, "scrchk.exe");
+                installFile(2, "ConfigEditer.exe");
+                installFile(3, "ConfigEditerServer.exe");
+                installFile(4, "Autorun.exe");
+                installFile(5, "KeyTool.exe");
+                installFile(6, "NetTool.exe");
+                installDll();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "安装失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show(string.Join("\n", stat.ToArray()), "安装完毕", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void installFile(int listIndex, string tmpFile, bool isDel = false)
+        {
+            if (checkedListBox1.GetItemCheckState(listIndex) == CheckState.Checked)
+            {
+                string toFile = textBox1.Text + tmpFile;
+                if (!isDel)
+                {
+                    if (File.Exists(toFile)) File.Delete(toFile);
+                    File.Copy(tmpFile, toFile);
+                    stat.Add("已复制：" + toFile);
+                }
+                else if (File.Exists(toFile))
+                {
+                    File.Delete(toFile);
+                    stat.Add("已删除：" + toFile);
+                }
+            }
+        }
+
+        private void installDll(bool isDel = false)
+        {
+            bool isInstall = false;
+            if (!isDel)
+            {
+                for (int i = 1; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (checkedListBox1.GetItemCheckState(i) == CheckState.Checked)
+                    {
+                        isInstall = true;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < dlls.Length; i++)
+            {
+                string toPath = textBox1.Text + dlls[i];
+                if (isInstall)
+                {
+                    if (File.Exists(toPath)) File.Delete(toPath);
+                    File.Copy(dlls[i], toPath);
+                    stat.Add("已复制：" + toPath);
+                }
+                else if (File.Exists(toPath))
+                {
+                    File.Delete(toPath);
+                    stat.Add("已删除：" + toPath);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stat.Clear();
+            try
+            {
+                installFile(0, "nyarukoeyesev.exe", true);
+                installFile(1, "NELauncher.exe", true);
+                installFile(1, "scrchk.exe", true);
+                installFile(2, "ConfigEditer.exe", true);
+                installFile(3, "ConfigEditerServer.exe", true);
+                installFile(4, "Autorun.exe", true);
+                installFile(5, "KeyTool.exe", true);
+                installFile(6, "NetTool.exe", true);
+                installDll(true);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "删除失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show(string.Join("\n", stat.ToArray()), "删除完毕", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < dlls.Length; i++)
+                {
+                    if (File.Exists(dlls[i])) File.Delete(dlls[i]);
+                }
+                for (int i = 0; i < exes.Length; i++)
+                {
+                    if (File.Exists(exes[i])) File.Delete(exes[i]);
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "临时文件清除失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
